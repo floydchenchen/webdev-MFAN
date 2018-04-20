bd8a423a4dfa021a7702ce2cac02d2a529ca5f72
陈士豪 <>
2018-04-20 08:05:06 -0500

    udpate package.json start

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var fullcalendar = require('fullcalendar');
-var fullCalendarScheduler = require('fullcalendar-scheduler');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
