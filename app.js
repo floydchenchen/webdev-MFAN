a0df7ae1487c24ef8ea998067d6f20523707f3d7
陈士豪 <>
2018-04-20 07:57:10 -0500

    finish 40%

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var fullcalendar = require('fullcalendar');
+var fullCalendarScheduler = require('fullcalendar-scheduler');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
